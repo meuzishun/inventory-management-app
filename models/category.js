d5f175b7f5545b1e7d9c38768f02a03a7dabdc45
Andrew Smith <meuzishun@gmail.com>
2023-03-06 07:41:26 -0500

    Create models

---
+++ models/category.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+CategorySchema.virtual('url').get(function () {
+  return `category/${this._id}`;
+});
