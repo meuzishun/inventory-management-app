bbb2e96864365717629a34dd9a31463c3fe479c3
Andrew Smith <meuzishun@gmail.com>
2023-03-08 09:42:25 -0500

    Write controller functions for category collection

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,39 +1,94 @@
+const asyncHandler = require('express-async-handler');
 const Category = require('../models/category');
+const Item = require('../models/item');
 
 // @desc    Create category
 // @route   POST /categories
 // @access  Private
-const createCategory = (req, res) => {
-  res.send('Create a new category');
-};
+const createCategory = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+
+  if (!name) {
+    res.status(400);
+    throw new Error('Please include a name');
+  }
+
+  if (!description) {
+    res.status(400);
+    throw new Error('Please include a description');
+  }
+
+  const category = await Category.create({
+    name,
+    description,
+  });
+
+  res.status(200).json(category);
+});
 
 // @desc    Get a single category
 // @route   GET /categories/:id
 // @access  Private
-const readCategory = (req, res) => {
-  res.send(`Read category with id ${req.params.id}`);
-};
+const readCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    res.status(400);
+    throw new Error('Category not found');
+  }
+
+  res.status(200).json(category);
+});
 
 // @desc    Get all categories
 // @route   GET /categories
 // @access  Private
-const readAllCategories = (req, res) => {
-  res.send('Read all categories');
-};
+const readAllCategories = asyncHandler(async (req, res) => {
+  const categories = await Category.find();
+
+  res.status(200).json(categories);
+});
 
 // @desc    Update category
 // @route   PUT /categories/:id
 // @access  Private
-const updateCategory = (req, res) => {
-  res.send(`Update category with id ${req.params.id}`);
-};
+const updateCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    res.status(400);
+    throw new Error('Category not found');
+  }
+
+  const updatedCategory = await Category.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedCategory);
+});
 
 // @desc    Delete category
 // @route   DELETE /categories/:id
 // @access  Private
-const deleteCategory = (req, res) => {
-  res.send(`Delete category with id ${req.params.id}`);
-};
+const deleteCategory = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  const itemsInCategory = await Item.find({ category: req.params.id });
+
+  if (!category) {
+    res.status(400);
+    throw new Error('Category not found');
+  }
+
+  if (itemsInCategory.length !== 0) {
+    res.status(500);
+    throw new Error('Cannot delete a category that has items');
+  }
+
+  await category.deleteOne();
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
   createCategory,
