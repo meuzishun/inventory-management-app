8bfd4b37cbc3a3effda7de55795f03b8e2f4c3ef
Andrew Smith <meuzishun@gmail.com>
2023-03-10 19:43:05 -0500

    Add validation and sanitization to item controllers

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,65 +1,106 @@
 const asyncHandler = require('express-async-handler');
 const Item = require('../models/item');
 const Category = require('../models/category');
+const { body, validationResult } = require('express-validator');
+
+const itemValidation = [
+  body('name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Please include a name.'),
+  body('description')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Please include a description.'),
+  body('category')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Please include a category.'),
+  body('price')
+    .isCurrency()
+    .withMessage('Please make sure price is a currency'),
+  body('quantity')
+    .not()
+    .isDecimal()
+    .withMessage('Quantity must be an integer.')
+    .isInt({ min: 0 })
+    .withMessage('Quantity cannot be negative.'),
+];
 
 // @desc    Create item
 // @route   POST /items
 // @access  Private
-const createItem = asyncHandler(async (req, res) => {
-  const { name, description, category, price, quantity } = req.body;
+const createItem = [
+  itemValidation,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  if (!name) {
-    res.status(400);
-    throw new Error('Please include a name');
-  }
+    if (!errors.isEmpty()) {
+      res.status(500).json(errors);
+      return;
+    }
 
-  if (!description) {
-    res.status(400);
-    throw new Error('Please include a description');
-  }
+    const { name, description, category, price, quantity } = req.body;
 
-  if (!category) {
-    res.status(400);
-    throw new Error('Please include a category');
-  }
+    // if (!name) {
+    //   res.status(400);
+    //   throw new Error('Please include a name');
+    // }
 
-  if (!price) {
-    res.status(400);
-    throw new Error('Please include a price');
-  }
+    // if (!description) {
+    //   res.status(400);
+    //   throw new Error('Please include a description');
+    // }
 
-  if (!quantity) {
-    res.status(400);
-    throw new Error('Please include a quantity');
-  }
+    // if (!category) {
+    //   res.status(400);
+    //   throw new Error('Please include a category');
+    // }
 
-  //* Use this if category is id
-  // const item = await Item.create({
-  //   name,
-  //   description,
-  //   category,
-  //   price,
-  //   quantity,
-  // });
+    // if (!price) {
+    //   res.status(400);
+    //   throw new Error('Please include a price');
+    // }
 
-  //* Use this if category is text
-  const foundCategory = await Category.findOne({ name: category });
-  if (!foundCategory) {
-    res.status(400);
-    throw new Error('Not a pre-existing category');
-  }
-  const categoryId = foundCategory._id.toString();
+    // if (!quantity) {
+    //   res.status(400);
+    //   throw new Error('Please include a quantity');
+    // }
 
-  const item = await Item.create({
-    name,
-    description,
-    category: categoryId,
-    price,
-    quantity,
-  });
+    //* Use this if category is id
+    // const item = await Item.create({
+    //   name,
+    //   description,
+    //   category,
+    //   price,
+    //   quantity,
+    // });
 
-  res.status(200).json(item);
-});
+    //* Use this if category is text
+
+    const foundCategory = await Category.findOne({ name: category });
+
+    if (!foundCategory) {
+      res.status(400);
+      throw new Error('Not a pre-existing category');
+    }
+
+    const categoryId = foundCategory._id.toString();
+
+    const item = await Item.create({
+      name,
+      description,
+      category: categoryId,
+      price,
+      quantity,
+    });
+
+    res.status(200).json(item);
+  }),
+];
 
 // @desc    Get a single item
 // @route   GET /items/:id
@@ -87,69 +128,83 @@ const readAllItems = asyncHandler(async (req, res) => {
 // @desc    Update item
 // @route   PUT /items/:id
 // @access  Private
-const updateItem = asyncHandler(async (req, res) => {
-  const { name, description, category, price, quantity } = req.body;
+const updateItem = [
+  itemValidation,
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  if (!name) {
-    res.status(400);
-    throw new Error('Please include a name');
-  }
-
-  if (!description) {
-    res.status(400);
-    throw new Error('Please include a description');
-  }
-
-  if (!category) {
-    res.status(400);
-    throw new Error('Please include a category');
-  }
-
-  if (!price) {
-    res.status(400);
-    throw new Error('Please include a price');
-  }
-
-  if (!quantity) {
-    res.status(400);
-    throw new Error('Please include a quantity');
-  }
-  const item = await Item.findById(req.params.id);
-
-  if (!item) {
-    res.status(400);
-    throw new Error('Item not found');
-  }
-
-  //* Use this if category is id
-  // const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
-  //   new: true,
-  // });
-
-  //* Use this if category is text
-  const foundCategory = await Category.findOne({ name: category });
-  if (!foundCategory) {
-    res.status(400);
-    throw new Error('Not a pre-existing category');
-  }
-  const categoryId = foundCategory._id.toString();
-
-  const updatedItem = await Item.findByIdAndUpdate(
-    req.params.id,
-    {
-      name,
-      description,
-      category: categoryId,
-      price,
-      quantity,
-    },
-    {
-      new: true,
+    if (!errors.isEmpty()) {
+      res.status(500).json(errors);
+      return;
     }
-  );
 
-  res.status(200).json(updatedItem);
-});
+    const { name, description, category, price, quantity } = req.body;
+
+    // if (!name) {
+    //   res.status(400);
+    //   throw new Error('Please include a name');
+    // }
+
+    // if (!description) {
+    //   res.status(400);
+    //   throw new Error('Please include a description');
+    // }
+
+    // if (!category) {
+    //   res.status(400);
+    //   throw new Error('Please include a category');
+    // }
+
+    // if (!price) {
+    //   res.status(400);
+    //   throw new Error('Please include a price');
+    // }
+
+    // if (!quantity) {
+    //   res.status(400);
+    //   throw new Error('Please include a quantity');
+    // }
+    // const item = await Item.findById(req.params.id);
+
+    // if (!item) {
+    //   res.status(400);
+    //   throw new Error('Item not found');
+    // }
+
+    //* Use this if category is id
+
+    // const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    // });
+
+    //* Use this if category is text
+
+    const foundCategory = await Category.findOne({ name: category });
+
+    if (!foundCategory) {
+      res.status(400);
+      throw new Error('Not a pre-existing category');
+    }
+
+    const categoryId = foundCategory._id.toString();
+
+    const updatedItem = await Item.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        description,
+        category: categoryId,
+        price,
+        quantity,
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json(updatedItem);
+  }),
+];
 
 // @desc    Delete category
 // @route   DELETE /items/:id
