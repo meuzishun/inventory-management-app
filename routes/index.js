370d6f523ca459fd9b1360b6d8f3cfae78b6ad2c
Andrew Smith <meuzishun@gmail.com>
2023-04-02 12:37:43 -0400

    Create home view and route to it

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.redirect('/categories');
+router.get('/', (req, res) => {
+  res.status(200).render('home');
 });
 
 module.exports = router;
