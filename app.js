6ef4e28236fb22f40456d4e3a46ce7624191b89a
Andrew Smith <meuzishun@gmail.com>
2023-04-10 19:34:49 -0400

    Use compression

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 const categoryRouter = require('./routes/categoryRouter');
 const itemRouter = require('./routes/itemRouter');
 const expressLayouts = require('express-ejs-layouts');
+const compression = require('compression');
 
 async function main() {
   try {
@@ -36,6 +37,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
